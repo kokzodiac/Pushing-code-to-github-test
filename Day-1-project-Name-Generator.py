d6c6bc7abe7fbb4ee6eb59393f01e5e01fa245e4
kok.zodiac <kok.zodiac@gmail.com>
2020-12-29 15:14:08 +1300

    name change

--- Day-1-project-Name-Generator.py
+++ Day-1-project-Name-Generator.py
@@ -1,4 +1,4 @@
 print("Wassup cunts")
 food = input("What do you eat for lunch you dipshit?\n")
-extra_food = input("What else do you have for lunch gay cunt?\n")
+extra_food = input("What else do you have for lunch gay cunt xdxdxdxdxd?\n")
 print("You cunt would be a " + food +" " + extra_food)
